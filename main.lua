f0ab4460ac86ef6707304313c60439149ad76c25
Sledmine <angelherxd@hotmail.com>
2020-06-14 18:15:01 -0600

    addded comments to main

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
+------------------------------------------------------------------------------
+-- Main
+-- Author: Sledmine
+-- Main entry for the application
+------------------------------------------------------------------------------
 local composer = require("composer")
 
+-- Load first application scene
 composer.gotoScene("scenes.home")
